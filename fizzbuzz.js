102d12ec7b0c953d64bd7256b483d423fb13f110
Krish Veera <krishvusf@gmail.com>
2022-03-08 18:33:11 -0500

    all done

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,10 @@
+// Welcome message to be shown as the page loads
 window.onload = function() {
     const userName = sessionStorage.getItem("userName");
     document.getElementById("welcomeMessage").innerHTML = "Welcome, " + userName;
 }
 
+//redeclaration of post function since it is being used later on to update scores
 function post(url, data) {
     data = JSON.stringify(data);
     return new Promise((resolve, reject) => {
@@ -17,17 +19,20 @@ function post(url, data) {
     });
 }
 
+//function that is implemented on the click of the button
 function increment() {
     let scores = sessionStorage.getItem("score");
     scores++;
-    sessionStorage.setItem("score", scores);
+    sessionStorage.setItem("score", scores); //restoring the new value back in the original scores variable
     let outputValue = FizzBuzz(scores);
     document.getElementById("output").innerHTML = outputValue;
     const dataToSend = { score: scores};
-    const userURL = sessionStorage.getItem("userURL")
-    post(userURL, dataToSend);
+    const userURL = sessionStorage.getItem("userURL");
+    post(userURL, dataToSend); //updating the user's new score back on the api
 }
  
+
+//actual fizzbuzz logic
 function FizzBuzz(score){
     if(score % 3 == 0 && score % 5 == 0){  
         // return "FizzBuzz";
--- loginPage.js
+++ loginPage.js
@@ -1,3 +1,4 @@
+//declaring get since it is used later on to get user's score from the api
 function get(url) {
     return new Promise((resolve, reject) => {
       const http = new XMLHttpRequest();
@@ -7,32 +8,33 @@ function get(url) {
       http.open("GET", url);
       http.send();
     });
-  }
-  
-  function post(url, data) {
-    data = JSON.stringify(data);
-    return new Promise((resolve, reject) => {
-      const http = new XMLHttpRequest();
-      http.onload = function() {
+}
+
+//declaring post function since it is used later on to create a new user on the api
+function post(url, data) {
+  data = JSON.stringify(data);
+  return new Promise((resolve, reject) => {
+    const http = new XMLHttpRequest();
+    http.onload = function() {
         resolve({ status: http.status, data: JSON.parse(http.response) });
       };
-      http.open("POST", url);
-      //Make sure that the server knows we're sending it json data.
-      http.setRequestHeader("Content-Type", "application/json");
-      http.send(data);
-    });
-  }
+    http.open("POST", url);
+    //Make sure that the server knows we're sending it json data.
+    http.setRequestHeader("Content-Type", "application/json");
+    http.send(data);
+  });
+}
+
+//function implemented on the clicking of the start button for the user to login to fizzbuzz page
+function loginUser() {
+  const apiLink = "http://basic-web.dev.avc.web.usf.edu/";
+  const userID = document.getElementById("usernameID").value;
+  const userURL = apiLink + userID;
+  sessionStorage.setItem("userURL", userURL); //use of sessionStorage to store the values to be used in the other JS file
   
-  function loginUser() {
-    const apiLink = "http://basic-web.dev.avc.web.usf.edu/";
-    const userID = document.getElementById("usernameID").value;
-    const userURL = apiLink + userID;
-    sessionStorage.setItem("userURL", userURL);
-    console.log(userURL);
-   
-      get(userURL).then(function(response) {
-            //Put all code that relies on the data from this request in here.
-            if(response.status == 200){
+    get(userURL).then(function(response) {
+          //Put all code that relies on the data from this request in here.
+          if(response.status == 200){
             const username = response.data.id; //The username that was requested. In this case it is "myUserName".
             const score = response.data.score; //The user's current score.
             sessionStorage.setItem("userName", username);
@@ -45,7 +47,6 @@ function get(url) {
             sessionStorage.setItem("userName", userID);
             sessionStorage.setItem("score", 0);
           }
-          window.location.assign("./fizzBuzz.html");
-      });
-     
-  }
+      window.location.assign("./fizzBuzz.html");
+    });
+}
